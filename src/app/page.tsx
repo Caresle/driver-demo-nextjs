8cfe6e754412b34e1a78f1ca37e5e3bea76caa37
Caresle <carabesle@gmail.com>
2025-01-18 14:29:33 -0600

    feat(pages): add components to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@/components/ui/button"
+import BetweenZustand from "./_components/between-components-zustand/between-zustand"
+import BetweenComponents from "./_components/between-components/between-section"
+import BetweenPages from "./_components/between-pages/between-pages"
+import InlineDriver from "./_components/inline/inline-driver"
+import NestedComponents from "./_components/nested-components/nested-components"
+import UsingModals from "./_components/usign-modals/using-modals"
+import WithServerActions from "./_components/with-server-actions/with-server-actions"
 
 export default function Home() {
 	return (
-		<div>
-			<Button>Click me</Button>
+		<div className="p-2 h-screen w-full flex flex-col gap-2">
+			<InlineDriver />
+			<BetweenComponents />
+			<BetweenZustand />
+			<NestedComponents />
+			<UsingModals />
+			<BetweenPages />
+			<WithServerActions />
 		</div>
 	)
 }
