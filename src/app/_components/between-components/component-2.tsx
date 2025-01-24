3deba719487c60f8e2ae056c857ae1cf1ab264d9
Carlos Estrada <161525647+CarlitosLT@users.noreply.github.com>
2025-01-24 15:27:09 -0600

    feat: add between section tutor component

--- src/app/_components/between-components/component-2.tsx
+++ src/app/_components/between-components/component-2.tsx
@@ -1,15 +1,34 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import TutorStep from "@/tutor/components/tutor-step"
 import React from "react"
 
 export default function Component2() {
 	return (
-		<div className="border rounded-lg p-2 bg-white">
-			<h3 className="font-semibold text-xl">Component 2</h3>
-			<div className="flex flex-col gap-2">
-				<Input placeholder="Enter your name" />
-				<Button variant={"secondary"}>Submit</Button>
+		<TutorStep
+			title="Card for component 2"
+			description="Im in the component 2"
+			order={3}
+		>
+			<div className="border rounded-lg p-2 bg-white">
+				<h3 className="font-semibold text-xl">Component 2</h3>
+				<div className="flex flex-col gap-2">
+					<TutorStep
+						title="Step in component 2"
+						description="Im in the component 2input"
+						order={4}
+					>
+						<Input placeholder="Enter your name" />
+					</TutorStep>
+					<TutorStep
+						title="Step in component 2"
+						description="Action button"
+						order={5}
+					>
+						<Button variant={"secondary"}>Submit</Button>
+					</TutorStep>
+				</div>
 			</div>
-		</div>
+		</TutorStep>
 	)
 }
--- src/app/_components/between-components/component-3.tsx
+++ src/app/_components/between-components/component-3.tsx
@@ -1,15 +1,34 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import TutorStep from "@/tutor/components/tutor-step"
 import React from "react"
 
 export default function Component3() {
 	return (
 		<div className="border rounded-lg p-2 bg-white">
 			<h3 className="font-semibold text-xl">Component 3</h3>
-			<div className="flex flex-col gap-2">
-				<Input placeholder="Enter your name" />
-				<Button variant={"secondary"}>Submit</Button>
-			</div>
+			<TutorStep
+				title="Title"
+				description="Ya me quede sin que hacer"
+				order={6}
+			>
+				<div className="flex flex-col gap-2">
+					<TutorStep
+						title="Title 2"
+						description="Pues es un input la neta"
+						order={7}
+					>
+						<Input placeholder="Enter your name" />
+					</TutorStep>
+					<TutorStep
+						title="Final step"
+						description="Pues es el final step papito"
+						order={8}
+					>
+						<Button variant={"secondary"}>Submit</Button>
+					</TutorStep>
+				</div>
+			</TutorStep>
 		</div>
 	)
 }
