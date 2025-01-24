d097bdf57c303d5b13e367db1a23a8b83f7f2926
Carlos Estrada <161525647+CarlitosLT@users.noreply.github.com>
2025-01-24 15:24:42 -0600

    feat: add tutor on inline-driver

--- src/app/_components/inline/inline-driver.tsx
+++ src/app/_components/inline/inline-driver.tsx
@@ -2,25 +2,48 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import React from "react"
 import Card from "../shared/card"
+import { TutorProvider } from "@/tutor/provider/tutor-provider"
+import TutorStep from "@/tutor/components/tutor-step"
+import TutorStepTrigger from "@/tutor/components/tutor-step-trigger"
 
 export default function InlineDriver() {
 	return (
-		<Card>
-			<div className="flex items-center gap-2">
-				<h2 className="font-semibold text-2xl">Inline Driver</h2>
-				<Button>Start</Button>
-			</div>
-			<p className="text-slate-500">
-				This is a demo of the inline driver. That is when driver is being used
-				in a single file and not between multiple Components
-			</p>
-			<div className="w-full bg-slate-50 border rounded-lg flex flex-col items-center gap-2 p-2">
-				<div className="flex flex-col gap-2 w-1/2 bg-white p-2 rounded-lg border">
-					<Input placeholder="Username" />
-					<Input placeholder="Password" type="password" />
-					<Button variant={"secondary"}>Login</Button>
+		<TutorProvider section="inline">
+			<Card>
+				<div className="flex items-center gap-2">
+					<h2 className="font-semibold text-2xl">Inline Driver</h2>
+					<TutorStepTrigger>Start</TutorStepTrigger>
 				</div>
-			</div>
-		</Card>
+				<p className="text-slate-500">
+					This is a demo of the inline driver. That is when driver is being used
+					in a single file and not between multiple Components
+				</p>
+				<div className="w-full bg-slate-50 border rounded-lg flex flex-col items-center gap-2 p-2">
+					<TutorStep
+						title="Login"
+						description="This is the login step"
+						order={1}
+					>
+						<div className="flex flex-col gap-2 max-w-2xl bg-white p-2 rounded-lg border mx-auto">
+							<TutorStep
+								title="Username"
+								description="This is the username step"
+								order={2}
+							>
+								<Input placeholder="Username" />
+							</TutorStep>
+							<TutorStep
+								title="Password"
+								description="This is the password step"
+								order={3}
+							>
+								<Input placeholder="Password" type="password" />
+							</TutorStep>
+							<Button variant={"secondary"}>Login</Button>
+						</div>
+					</TutorStep>
+				</div>
+			</Card>
+		</TutorProvider>
 	)
 }
